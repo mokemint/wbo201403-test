dafaec549fa447138005766ba062bb85ae893cda
conti <contiki9@gmail.com>
2014-03-16 16:29:56 +0900

    クラス追加

--- twentyfourteen/single.php
+++ twentyfourteen/single.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary add" class="content-area">
 		<div id="content" class="site-content" role="main">
 			<?php
 				// Start the Loop.
